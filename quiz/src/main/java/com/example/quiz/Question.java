fcf7f94e122cf681ca2ad0ae7389b86bbd71953a
Saritha Rajamanickam <saritha.r@KGFSL.COM>
2018-05-22 18:58:45 +0530

    first

---
+++ quiz/src/main/java/com/example/quiz/Question.java
@@ -0,0 +1,69 @@
+package com.example.quiz;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+class Question
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+     @Column
+    private String language;
+     @Column
+    private String question;
+    
+    @Column
+    private String code;
+
+    @Column
+    private String options[];
+
+    public Integer getId() {
+		return id;
+	}
+
+    public void setId(Integer id) {
+		this.id = id;
+	}
+
+    public String getlanguage() {
+		return language;
+	}
+
+	public void setlanguage(String language) {
+		this.language = language;
+	}
+
+    public String getquestion() {
+		return question;
+	}
+
+	public void setquestion(String question) {
+		this.question = question;
+	}
+
+    public String getcode() {
+		return code;
+	}
+
+	public void setcode(String code) {
+		this.code = code;
+	}
+    
+    public String[] getoptions(){
+        return options;
+    }
+
+    public void setoptions(String[] options) {
+    this.options = options;
+}
+//  @OneToOne(cascade = CascadeType.ALL)
+    // @JoinColumn(name = "book_detail_id")
+
+}
